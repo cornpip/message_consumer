992f3924cfcc0316b0c15478a26a174f2c1fc696
cornpip <chdnjf1375@naver.com>
2023-09-11 01:52:55 +0900

    init : MongoDbConsumerApplication, docker-compose

---
+++ src/main/java/com/message/kafka_consumer_mongodb/MongoDbConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.message.kafka_consumer_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/message/kafka_consumer_mongodb/KafkaConsumerMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message.kafka_consumer_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
